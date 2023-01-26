eba645fb09259a583a5cf1c283accd1c323013de
Matheus <mf635097@gmail.com>
2023-01-26 07:51:27 -0300

    Fixed style and wrong writing in component

--- frontend/src/components/Results/index.tsx
+++ frontend/src/components/Results/index.tsx
@@ -4,7 +4,7 @@ import * as Styled from './styles';
 export const Results = () => {
   return (
     <Styled.Container>
-      <Title text="Resultados" />
+      <Title text="Resultado" />
     </Styled.Container>
   );
 };
