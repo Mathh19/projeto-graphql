fd6935d28058fd196714168713d551ffe1cda6eb
Matheus <mf635097@gmail.com>
2023-01-26 07:56:36 -0300

    Routing Users

--- frontend/src/pages/Users/index.tsx
+++ frontend/src/pages/Users/index.tsx
@@ -1,9 +1,55 @@
-import * as Styled from './styles';
+import { ListUser } from '../../components/ListUser';
+import { NewUser } from '../../components/NewUser';
+import { Results } from '../../components/Results';
+import { SearchUser } from '../../components/SearchUser';
+import { Section } from '../../components/Section';
+import { Sections } from '../../components/Sections';
+import { TabSections } from '../../components/TabsSections';
 
 export const UsersPage = () => {
+  const tabs = [
+    {
+      id: '1',
+      content: 'Lista',
+      link: '#list',
+    },
+    {
+      id: '2',
+      content: 'Consulta',
+      link: '#search',
+    },
+    {
+      id: '3',
+      content: 'Novo',
+      link: '#add',
+    },
+    {
+      id: '4',
+      content: 'Alterar',
+      link: '#update',
+    },
+    {
+      id: '5',
+      content: 'Excluir',
+      link: '#delete',
+    },
+  ];
+
   return (
-    <Styled.Container>
-      <h2>Olá</h2>
-    </Styled.Container>
+    <TabSections tabs={tabs}>
+      <Sections>
+        <Section id="list">
+          <ListUser />
+        </Section>
+        <Section id="search">
+          <SearchUser />
+          <Results />
+        </Section>
+        <Section id="add">
+          <NewUser />
+          <Results />
+        </Section>
+      </Sections>
+    </TabSections>
   );
 };
