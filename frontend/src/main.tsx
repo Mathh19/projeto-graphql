dcf5eb2d59eb6adfb6b97117e348497c3d29bfd6
Matheus <mf635097@gmail.com>
2023-02-01 20:19:05 -0300

    Add graphql client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
 import { AuthPage } from './pages/AuthPage';
-import App from './App';
 import { UsersPage } from './pages/Users';
 import { ProfilePage } from './pages/Profiles';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './graphql/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<App />}>
-            <Route path="/" element={<AuthPage />} />
-            <Route path="/users" element={<UsersPage />} />
-            <Route path="/profiles" element={<ProfilePage />} />
-          </Route>
-        </Routes>
-        <GlobalStyles />
-      </BrowserRouter>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<App />}>
+              <Route path="/" element={<AuthPage />} />
+              <Route path="/users" element={<UsersPage />} />
+              <Route path="/profiles" element={<ProfilePage />} />
+            </Route>
+          </Routes>
+          <GlobalStyles />
+        </BrowserRouter>
+      </ApolloProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
