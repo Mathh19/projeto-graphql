6c2b7c750edc73658e770674ec4f1e3353cc5285
Matheus <mf635097@gmail.com>
2023-02-06 14:47:39 -0300

    Added for button to be able to add data to table and added error component

--- frontend/src/components/ListData/index.tsx
+++ frontend/src/components/ListData/index.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { GET_PROFILES, GET_USERS } from '../../graphql/querys';
 import { DataFormComponents } from '../../sharedTypes/dataFormComponents';
+import { ErrorMessage } from '../ErrorMessage';
 import { Loading } from '../Loading';
 import { TableData } from '../TableData';
 import * as Styled from './styles';
@@ -10,19 +12,28 @@ type ListDataProps = {
 } & DataFormComponents;
 
 export const ListData = ({ columns, userOrProfile }: ListDataProps) => {
+  const [usersProfiles, setUsersProfiles] = useState();
   const { data, loading, error } = useQuery(
     userOrProfile === 'user' ? GET_USERS : GET_PROFILES,
   );
 
   if (loading) return <Loading />;
-  if (error) return <p>Error : {error.message}</p>;
+  if (error) return <ErrorMessage message={`Error : ${error.message}`} />;
+
+  const handleClick = () => {
+    setUsersProfiles(data);
+  };
 
   return (
     <Styled.Container>
-      <Styled.Button>
+      <Styled.Button onClick={handleClick}>
         Obter {`${userOrProfile === 'user' ? 'Usuários' : 'Perfis'}`}
       </Styled.Button>
-      <TableData columns={columns} userOrProfile={userOrProfile} data={data} />
+      <TableData
+        columns={columns}
+        userOrProfile={userOrProfile}
+        data={usersProfiles}
+      />
     </Styled.Container>
   );
 };
