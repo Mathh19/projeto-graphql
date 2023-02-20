88a74d952035331eeeb3163141a80158dbd8b32b
Matheus <mf635097@gmail.com>
2023-02-20 16:39:48 -0300

    Changing component results to receive a children of results for each component that uses it

--- frontend/src/components/Results/index.tsx
+++ frontend/src/components/Results/index.tsx
@@ -1,10 +1,15 @@
 import { Title } from '../Title';
 import * as Styled from './styles';
 
-export const Results = () => {
+type ResultsProps = {
+  children: React.ReactNode;
+};
+
+export const Results = ({ children }: ResultsProps) => {
   return (
     <Styled.Container>
       <Title text="Resultado" />
+      {children}
     </Styled.Container>
   );
 };
