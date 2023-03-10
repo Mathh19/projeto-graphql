121f96d69b15cf17f64ece52f5d7aead477bad62
Matheus <mf635097@gmail.com>
2023-03-10 19:01:23 -0300

    Add querys to user and profile

--- frontend/src/graphql/mutations.ts
+++ frontend/src/graphql/mutations.ts
@@ -20,5 +20,5 @@ export const LOGIN_USER = gql`
       email
       token
     }
-}
+  }
 `;
--- frontend/src/graphql/querys.ts
+++ frontend/src/graphql/querys.ts
@@ -13,12 +13,40 @@ export const GET_USERS = gql`
   }
 `;
 
+export const GET_USER = gql`
+  query User($filter: UserFilter!) {
+    user(filter: $filter) {
+      id
+      name
+      email
+      profiles {
+        id
+        label
+      }
+    }
+  }
+`;
+
 export const GET_PROFILES = gql`
   query Profiles {
     profiles {
       id
       name
       label
+    }
+  }
+`;
+export const GET_PROFILE = gql`
+  query Profile($filter: ProfileFilter!) {
+    profile(filter: $filter) {
+      id
+      name
+      label
+      users {
+        id
+        email
+        name
+      }
     }
   }
 `;
